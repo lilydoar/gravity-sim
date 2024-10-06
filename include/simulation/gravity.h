b53d5bef89a71768e595a2ad23ef2e9cbc91c4c9
Lily Doar <lilydoar4@gmail.com>
2024-10-06 00:36:28 -0700

    add gravity header

--- include/simulation/gravity.h
+++ include/simulation/gravity.h
@@ -1,4 +1,29 @@
 #ifndef GRAVITY_H
 #define GRAVITY_H
 
+#include "math/vector.h"
+
+// Gravitational constant (G) in N⋅m^2/kg^2
+#define GRAVITATIONAL_CONSTANT 6.67430e-11
+
+// Calculates gravitational force between two objects
+// p1, p2: Position vectors
+// m1, m2: Masses of objects
+// G: Gravitational constant
+// Returns: Force magnitude
+double calculate_force(Vec2 p1, Vec2 p2, double m1, double m2, double G)
+{
+    // Calculate the vector from p2 to p1
+    Vec2 r = vec2_sub(p1, p2);
+
+    // Calculate the square of the distance between the two objects
+    double distance_squared = vec2_dot(r, r);
+
+    // Apply Newton's law of universal gravitation:
+    // F = G * (m1 * m2) / r^2
+    // where F is the force, G is the gravitational constant,
+    // m1 and m2 are the masses, and r is the distance between the objects
+    return G * m1 * m2 / distance_squared;
+}
+
 #endif // GRAVITY_H
